7964f3c3412ae193a9f0bae0563613d0070686b1
Orrin Manning <orrin@orrin.dev>
2026-03-14 18:37:40 +0000

    Add design mockup and magazine design system

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
-from fastapi import FastAPI
+from datetime import date
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="bananabread.blog")
 
+app.mount("/static", StaticFiles(directory="app/web/static"), name="static")
+templates = Jinja2Templates(directory="app/web/templates")
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome to bananabread.blog"}
+
+def get_date() -> str:
+    return date.today().strftime("%A, %-d %B %Y")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context={"date": get_date()},
+    )
